6f9cf1edd44806dc94b2d884e3de6ef9a81c5f70
Raymond Idengeli <ray.idengeli@gmail.com>
2021-07-05 16:05:02 +0100

    front end 90% done

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 function Main() {
     return (
-        <div className="max w-10/12 mx-auto my-3">
-            <div className="bg-gray-100  text-bold w-96 flex">
+        <div className="w-full mx-auto my-3 justify-evenly flex-wrap flex">
+            <div className="shadow-xl m-5 rounded-bl-3xl border-l border-t-0 border-r border-b text-bold w-96 flex">
                 <div className="">
                     <img src="/img/food.jpg" className="mx-auto rounded-tr-3xl mb-6 rounded-bl-3xl" alt="" />
                     <div className="flex text-2xl px-3 mb-2 font-bold justify-between">
@@ -19,17 +19,140 @@ function Main() {
                             <i className="fas fa-star"></i>
                             <i className="fas fa-star"></i>
                             <i className="fas fa-star"></i>
-                            <i className="fas fa-star"></i>
-                           
+                            <i className="fas fa-star"></i> 
                         </div>
                         <div className="bg-custom-orange rounded-tl-2xl px-11 py-7">
                         <i className="fas fa-plus text-white"></i>
                         </div>
                     </div>
-                    
                 </div>
+            </div>
+            <div className="shadow-xl m-5 rounded-bl-3xl border-l border-t-0 border-r border-b text-bold w-96 flex">
+                <div className="">
+                    <img src="/img/food.jpg" className="mx-auto rounded-tr-3xl mb-6 rounded-bl-3xl" alt="" />
+                    <div className="flex text-2xl px-3 mb-2 font-bold justify-between">
+                        <div className="">Rose Muffen</div>
+                        <div>12$</div>
+                    </div>
+                    <div className="p-3">Lorem ipsum dolor sit amet consectetur</div>
+                    <div className="mb-4 p-3">
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam in enim ut? Id, et rem.
+                    </div>
+                    <div className="flex justify-between items-center">
+                        <div className="flex pl-7 py-9 text-custom-orange">
+                            <i className="fas fa-star"></i>
+                            <i className="fas fa-star"></i>
+                            <i className="fas fa-star"></i>
+                            <i className="fas fa-star"></i> 
+                        </div>
+                        <div className="bg-custom-orange rounded-tl-2xl px-11 py-7">
+                        <i className="fas fa-plus text-white"></i>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className="shadow-xl m-5 rounded-bl-3xl border-l border-t-0 border-r border-b text-bold w-96 flex">
+                <div className="">
+                    <img src="/img/food.jpg" className="mx-auto rounded-tr-3xl mb-6 rounded-bl-3xl" alt="" />
+                    <div className="flex text-2xl px-3 mb-2 font-bold justify-between">
+                        <div className="">Rose Muffen</div>
+                        <div>12$</div>
+                    </div>
+                    <div className="p-3">Lorem ipsum dolor sit amet consectetur</div>
+                    <div className="mb-4 p-3">
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam in enim ut? Id, et rem.
+                    </div>
+                    <div className="flex justify-between items-center">
+                        <div className="flex pl-7 py-9 text-custom-orange">
+                            <i className="fas fa-star"></i>
+                            <i className="fas fa-star"></i>
+                            <i className="fas fa-star"></i>
+                            <i className="fas fa-star"></i> 
+                        </div>
+                        <div className="bg-custom-orange rounded-tl-2xl px-11 py-7">
+                        <i className="fas fa-plus text-white"></i>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className="shadow-xl m-5 rounded-bl-3xl border-l border-t-0 border-r border-b text-bold w-96 flex">
+                <div className="">
+                    <img src="/img/food.jpg" className="mx-auto rounded-tr-3xl mb-6 rounded-bl-3xl" alt="" />
+                    <div className="flex text-2xl px-3 mb-2 font-bold justify-between">
+                        <div className="">Rose Muffen</div>
+                        <div>12$</div>
+                    </div>
+                    <div className="p-3">Lorem ipsum dolor sit amet consectetur</div>
+                    <div className="mb-4 p-3">
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam in enim ut? Id, et rem.
+                    </div>
+                    <div className="flex justify-between items-center">
+                        <div className="flex pl-7 py-9 text-custom-orange">
+                            <i className="fas fa-star"></i>
+                            <i className="fas fa-star"></i>
+                            <i className="fas fa-star"></i>
+                            <i className="fas fa-star"></i> 
+                        </div>
+                        <div className="bg-custom-orange rounded-tl-2xl px-11 py-7">
+                        <i className="fas fa-plus text-white"></i>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className="shadow-xl m-5 rounded-bl-3xl border-l border-t-0 border-r border-b text-bold w-96 flex">
+                <div className="">
+                    <img src="/img/food.jpg" className="mx-auto rounded-tr-3xl mb-6 rounded-bl-3xl" alt="" />
+                    <div className="flex text-2xl px-3 mb-2 font-bold justify-between">
+                        <div className="">Rose Muffen</div>
+                        <div>12$</div>
+                    </div>
+                    <div className="p-3">Lorem ipsum dolor sit amet consectetur</div>
+                    <div className="mb-4 p-3">
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam in enim ut? Id, et rem.
+                    </div>
+                    <div className="flex justify-between items-center">
+                        <div className="flex pl-7 py-9 text-custom-orange">
+                            <i className="fas fa-star"></i>
+                            <i className="fas fa-star"></i>
+                            <i className="fas fa-star"></i>
+                            <i className="fas fa-star"></i> 
+                        </div>
+                        <div className="bg-custom-orange rounded-tl-2xl px-11 py-7">
+                        <i className="fas fa-plus text-white"></i>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className="shadow-xl m-5 rounded-bl-3xl border-l border-t-0 border-r border-b text-bold w-96 flex">
+                <div className="">
+                    <img src="/img/food.jpg" className="mx-auto rounded-tr-3xl mb-6 rounded-bl-3xl" alt="" />
+                    <div className="flex text-2xl px-3 mb-2 font-bold justify-between">
+                        <div className="">Rose Muffen</div>
+                        <div>12$</div>
+                    </div>
+                    <div className="p-3">Lorem ipsum dolor sit amet consectetur</div>
+                    <div className="mb-4 p-3">
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam in enim ut? Id, et rem.
+                    </div>
+                    <div className="flex justify-between items-center">
+                        <div className="flex pl-7 py-9 text-custom-orange">
+                            <i className="fas fa-star"></i>
+                            <i className="fas fa-star"></i>
+                            <i className="fas fa-star"></i>
+                            <i className="fas fa-star"></i> 
+                        </div>
+                        <div className="bg-custom-orange rounded-tl-2xl px-11 py-7">
+                        <i className="fas fa-plus text-white"></i>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div className="">
 
             </div>
+            
+            
 
 
         </div>
